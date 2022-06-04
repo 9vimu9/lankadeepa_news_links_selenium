8afe5fd8b86d429ac28bcc7438b3bcd68394d6f1
vim <9wimu9@gmail.com>
2022-06-04 21:26:21 +0530

    add all links and types

--- src/news_links.py
+++ src/news_links.py
@@ -7,7 +7,20 @@ from selenium.webdriver.common.by import By
 
 logging.getLogger().setLevel(logging.INFO)
 
-BASE_URL = 'https://www.lankadeepa.lk/top_story/10/'
+BASE_URL_NEWS = 'https://www.lankadeepa.lk/top_story/10/'
+BASE_URL_KNOWLEDGE = 'https://www.lankadeepa.lk/encyclopedia/17'
+BASE_URL_FOREIGN = 'https://www.lankadeepa.lk/world_news/14'
+BASE_URL_FEATURED = 'https://www.lankadeepa.lk/sunday/rasawitha/57'#didnt work
+BASE_URL_CELEBRITY = 'https://www.saaravita.lk/bollyhollywood/105'#didnt work
+BASE_URL_TRAVEL = 'https://www.saaravita.lk/travel/110'#didnt work
+
+
+ARTCLE_TYPE_KNOWLEDGE = 0
+ARTCLE_TYPE_FOREIGN = 1
+ARTCLE_TYPE_FEATURED = 2
+ARTCLE_TYPE_CELEBRITY = 3
+ARTCLE_TYPE_TRAVEL = 4
+ARTCLE_TYPE_NEWS = 5
 
 
 def firefox_example():
@@ -27,16 +40,15 @@ def firefox_example():
     browser = webdriver.Firefox(options=firefox_options)
     logging.info('Initialized firefox browser..')
 
-    browser.get(BASE_URL)
-    logging.info('Accessed %s ..', BASE_URL)
+    browser.get(BASE_URL_NEWS)
+    logging.info('Accessed %s ..', BASE_URL_NEWS)
 
     logging.info('Page title: %s', browser.title)
 
     elems = browser.find_elements(By.CLASS_NAME,'news-title')
-    print(len(elems))
 
-    # for elem in elems:
-        # print(elem.get_attribute("href"))
+    for elem in elems:
+        print(elem.get_attribute("href"))
 
 
     browser.quit()
