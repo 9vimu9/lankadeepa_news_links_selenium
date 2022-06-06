d1a5f17ec55dbfae3e7ef5488dfe8bd47232a211
vim <9wimu9@gmail.com>
2022-06-06 23:02:39 +0530

    add mysql

---
+++ src/support/database/Connection.py
@@ -0,0 +1,66 @@
+import string
+import mysql.connector
+from mysql.connector import Error
+
+class Connection:
+    
+    def __init__(self):
+        self.__connection = None
+        self.__cursor = None
+        self.__create()
+    
+
+    def __create(self):
+        try:
+            connection = mysql.connector.connect(
+                host = 'mydb', 
+                database='wiki_sinhala_paragraphs',
+                user = 'root', 
+                password = 'root', 
+                port = 3306
+                )
+            if connection.is_connected():
+                self.__connection = connection
+                cursor = connection.cursor()
+                self.__cursor = cursor
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+       
+        
+    
+    def __close(self):
+            
+        if self.__connection.is_connected():
+            self.__cursor.close()
+            self.__connection.close()
+    
+
+    def execute(self,query:string):
+            self.__cursor.execute(query)
+            record =  self.__cursor.fetchall()
+            self.__close()
+            return record
+
+
+
+
+    @staticmethod
+    def query(query:string):
+        return (Connection()).execute(query)
+
+
+
+
+# The Google Python Style Guide has the following convention:
+    # module_name, 
+    # package_name, 
+    # ClassName, 
+    # method_name, 
+    # ExceptionName, 
+    # function_name, 
+    # GLOBAL_CONSTANT_NAME, 
+    # global_var_name, 
+    # instance_var_name, 
+    # function_parameter_name, 
+    # local_var_name
--- src/test.py
+++ src/test.py
@@ -4,6 +4,9 @@ from sites.Foreign import Foreign
 from sites.TopStory import TopStory
 from sites.TravelNews import TravelNews
 
+import mysql.connector
+from support.database.Connection import Connection
+
 
 if __name__ == '__main__':
     
@@ -12,4 +15,16 @@ if __name__ == '__main__':
     # news = Featured()
     # news = TopStory()
     news = Foreign()
-    print(news.getLinks())
+    # print(news.getLinks())
+
+    
+    # conn = mysql.connector.connect(host = 'mydb', user = 'root', password = 'root', port = 3306)
+
+    # cursor = conn.cursor()
+    # databases = ("show databases")
+    # cursor.execute(databases)
+    # for (databases) in cursor:
+    #     print (databases[0])
+
+    result = Connection.query("select database();")
+    print(result)
