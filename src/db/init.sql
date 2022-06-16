c667fc4e6c73a1d5f1adc622b719c01f9b3a676b
vim <9wimu9@gmail.com>
2022-06-16 23:26:13 +0530

    add valubale comments

--- src/db/init.sql
+++ src/db/init.sql
@@ -7,8 +7,8 @@ CREATE TABLE `articles` (
   `url` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `category` smallint(5) unsigned DEFAULT NULL,
   `approved` smallint(5) unsigned NOT NULL DEFAULT '2',
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -20,8 +20,8 @@ CREATE TABLE `paragraphs` (
   `order` smallint(5) unsigned NOT NULL,
   `approved` smallint(5) unsigned NOT NULL DEFAULT '2',
   `no_more_questions` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -37,6 +37,8 @@ ALTER TABLE paragraphs MODIFY COLUMN paragraph longtext  CHARACTER SET utf8mb4 C
 ALTER TABLE articles MODIFY COLUMN title mediumtext  CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
 ALTER TABLE articles MODIFY COLUMN url mediumtext  CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
 
+-- alter table articles ADD UNIQUE(url(700));
+
 -- CREATE UNIQUE INDEX title_unique_index ON articles (title(600));
 -- CREATE INDEX paragraphs_index ON paragraphs (`order`);
 
@@ -48,3 +50,12 @@ ALTER TABLE articles MODIFY COLUMN url mediumtext  CHARACTER SET utf8mb4 COLLATE
 
 -- export
 -- docker exec docker-tutorial_db_1 /usr/bin/mysqldump -u root --password=root wiki_sinhala_paragraphs > backup.sql
+
+-- mysql -uroot -proot
+-- use wiki_sinhala_paragraphs;
+-- select count(id),category from articles group by category;
+-- select count(id) from articles;
+
+
+-- to remove duplicate entries
+-- DELETE t1 FROM articles t1 JOIN articles t2 ON t2.url = t1.url AND t2.id < t1.id
--- src/support/Constant.py
+++ src/support/Constant.py
@@ -7,4 +7,12 @@ class Constant:
     ARTICLE_CATEGORY_TRAVEL = 4
     ARTICLE_CATEGORY_NEWS = 5
 
-    MAX_PAGES_FOR_ARTICLE = 1
+    MAX_PAGES_FOR_ARTICLE = 150
+    # 12 for knowlage
+    #  220 for celeb
+    #  500 for foreign
+    #  500 for news
+    #  13 for travel
+    #  91 for featured
+    #1 equals to 30 articles
+
