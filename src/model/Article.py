85d6d25ba9bc1400fa9b67bbe3c0584a36f8f535
vim <9wimu9@gmail.com>
2022-06-17 12:51:20 +0530

    create get_fresh_article

--- src/model/Article.py
+++ src/model/Article.py
@@ -1,10 +1,14 @@
 from model.Model import Model
-from support.article import Article
+from support.article.Article import Article as ArticleDTO
 from support.database.Connection import Connection
 
 
 class Article(Model):
 
-    def insert(self,article:Article):
+    def insert(self,article:ArticleDTO):
         Connection.insert("articles",article.dic())
-     
+    
+    def get_fresh_article(self,category:int):
+        query = "SELECT title,url,category FROM articles WHERE paragraphs_added = 0 AND category = "+str(category)+" LIMIT 1";
+        article = (Connection.query(query))[0]
+        return ArticleDTO(article[1],article[2],article[3])
